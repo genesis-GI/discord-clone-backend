81c6bad922aecd7a9991d8ac81d902874e1d887b
lukasWastaken <l.leutgeb@students.htl-leonding.ac.at>
2025-03-03 13:08:00 +0100

    cleaned up main.go

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main(){
     r.GET("/", func(c *gin.Context){
         c.String(200, "Hello World!")
     })
-    color.Cyan("[INFO] Routes initialized...")
+    
 
     color.Green("[SUCCESS] Server running on http://127.0.0.1:8050")
     r.Run(":8050")
