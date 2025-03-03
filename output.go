d67a04f34cca09265a9c3666dc623f59c2ff2066
Dyzody <david.schilcher11@gmx.at>
2025-03-03 13:08:02 +0100

    Created more output functions

--- output.go
+++ output.go
@@ -7,3 +7,19 @@ import (
 func warn(message string) {
 	color.Yellow("[WARN] " + message)
 }
+
+func info(message string) {
+	color.Cyan("[INFO] " + message)
+}
+
+func neutral(message string) {
+	color.White("[OUTPUT] " + message)
+}
+
+func success(message string) {
+	color.Green("[SUCCESS] " + message)
+}
+
+func errormsg(message string) {
+	color.Red("[ERROR] " + message)
+}
